3486bc07443c086b2dc01695fdfe53b7001b397b
Bahar <bahareh.abavi1981@gmail.com>
2026-01-16 22:34:26 +0100

    feat: add project structure and math functionality with unit tests

---
+++ src/MathApp/Calculator.cs
@@ -0,0 +1,18 @@
+namespace MathApp;
+
+/// <summary>
+/// En simpel lommeregner til at demonstrere unit testing.
+/// </summary>
+public class Calculator
+{
+    /// <summary>
+    /// Lægger to heltal sammen.
+    /// </summary>
+    /// <param name="a">Det første tal.</param>
+    /// <param name="b">Det andet tal.</param>
+    /// <returns>Summen af a og b.</returns>
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
